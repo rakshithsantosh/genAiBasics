89abeb11814a56d556040d6c4e78bf9164c6b622
rakshith_s <rakshithsantosh@gmail.com>
2025-04-28 12:50:28 +0530

    creating text output from prompt

--- basics/index.js
+++ basics/index.js
@@ -1,1 +1,17 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  const prompt = " write a story abount reactjs for beginners";
+  const result = await model.generateContent(prompt);
+
+  const response = await result.response;
+  const text = response.text();
+
+  console.log(text);
+}
+
+run();
